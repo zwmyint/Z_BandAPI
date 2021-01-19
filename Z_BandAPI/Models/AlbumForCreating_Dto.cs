f7c342fa2d899dbb90a743883638c56e64425e5b
ZAW WIN MYINT <45326766+zwmyint@users.noreply.github.com>
2021-01-19 23:07:53 +0800

    20210119-2307

--- Z_BandAPI/Models/AlbumForCreating_Dto.cs
+++ Z_BandAPI/Models/AlbumForCreating_Dto.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Z_BandAPI.ValidationAttributes;
 
 namespace Z_BandAPI.Models
 {
-    public class AlbumForCreating_Dto
+    [TitleAndDescriptionAttribute] // class level (overrided)
+    public class AlbumForCreating_Dto // : IValidatableObject
     {
+        [Required]
+        [MaxLength(200)]
         public string Title { get; set; }
+
+        [MaxLength(400)]
         public string Description { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if(Title == Description)
+        //    {
+        //        yield return new ValidationResult("The title and description need to be different.", new[] { "AlbumForCreation_Dto" });
+        //    }
+        //}
+
+
     }
 }
---
+++ Z_BandAPI/ValidationAttributes/TitleAndDescriptionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Z_BandAPI.Models;
+
+namespace Z_BandAPI.ValidationAttributes
+{
+    public class TitleAndDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var album = (AlbumForCreating_Dto)validationContext.ObjectInstance;
+
+            if (album.Title == album.Description)
+            {
+                return new ValidationResult("The title and the description need to be different", new[] { "AlbumForCreating_Dto" });
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
